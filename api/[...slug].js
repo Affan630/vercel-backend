d3c2a08f8d4255a051b179a1de3b529eda892532
affanmansoor <affanmansoor630@gmail.com>
2025-10-02 09:07:04 +0500

    some fixes

--- api/[...slug].js
+++ api/[...slug].js
@@ -1,6 +1,12 @@
 const app = require("../../server");
 const serverless = require("serverless-http");
 
-module.exports = serverless(app);
-
-
+module.exports = async (req, res) => {
+  try {
+    const handler = serverless(app);
+    return await handler(req, res);
+  } catch (err) {
+    console.error("Serverless function crashed:", err);
+    res.status(500).json({ error: "Internal Server Error", details: err.message });
+  }
+};
